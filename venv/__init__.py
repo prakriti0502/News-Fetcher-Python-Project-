ca0f5f92b89013f3f2d6545a5665de592faae489
prakriti0502 <Prakriti.sagar@icloud.com>
2018-10-08 11:17:24 +0530

    done

--- venv/__init__.py
+++ venv/__init__.py
@@ -7,40 +7,40 @@ from bs4 import BeautifulSoup
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/getNews')
 def index():
     return render_template('index.html')
 
 
-@app.route('/search', methods =['POST'])
+@app.route('/search', methods=['POST'])
 def search():
-    search = request.form['search']
-    msg=""
-    if search == "":
+    check = request.form['search']
+    show=""
+    if check == "":
         return render_template('index.html')
     else:
-        match = re.search(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)',search)
+        match = re.search(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)',check)
         if match:
-            N = NewsMe(search)
-            headline = []
-            links = []
-            for i in N.headlines():
-                headline.append(i[0])
-                links.append(i[1])
-            new_match = re.search(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)', links[1])
+            obj = NewsMe(check)
+            newsshow = []
+            values = []
+            for i in obj.headlines():
+                newsshow.append(i[0])
+                values.append(i[1])
+            new_match = re.search(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)', values[1])
             if new_match:
                 new = ""
             else:
-                if search[-1] == "/":
-                    new = search[0:-1]
+                if check[-1] == "/":
+                    new = check[0:-1]
                 else:
-                    new = search
+                    new = check
 
-            return render_template('search.html', headline=headline, links=links, search=search, new=new)
+            return render_template('search.html', headline=newsshow, links=values, search=check, new=new)
         else:
-            msg = "Enter valid Url"
-            return render_template("index.html", msg=msg)
+            show = "Enter valid Url"
+            return render_template("index.html", msg=show)
 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
